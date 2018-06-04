e593efdfbe504c2e22752d14abc9c8428a0a0727
Jim Crist <jiminy.crist@gmail.com>
2018-06-04 13:49:08 -0500

    Update dask commands

--- dask_yarn/cli/dask_yarn_scheduler.py
+++ dask_yarn/cli/dask_yarn_scheduler.py
@@ -1,17 +1,64 @@
-import click
+from __future__ import print_function, division, absolute_import
+
+import sys
+
 import skein
+from distributed import Scheduler
+from distributed.utils import ignoring
+from distributed.cli.utils import install_signal_handlers, uri_from_host_port
+from distributed.proctitle import (enable_proctitle_on_children,
+                                   enable_proctitle_on_current)
+from tornado.ioloop import IOLoop
 
 
-@click.command()
+if sys.version_info.major > 2:
+    from urllib.parse import urlparse
+else:
+    from urlparse import urlparse
+
+
 def main():
-    print('hello from scheduler')
-    client = skein.ApplicationClient.connect_to_current()
-    loop = tornado.ioloop.IOLoop.current()
-    scheduler = dask.distributed.Scheduler(loop=loop)
-    scheduler.start()
-    print(scheduler.address)
-    client.kv['scheduler-address'] = scheduler.address
-    loop.start()
+    app_client = skein.ApplicationClient.from_current()
+
+    enable_proctitle_on_current()
+    enable_proctitle_on_children()
+
+    if sys.platform.startswith('linux'):
+        import resource   # module fails importing on Windows
+        soft, hard = resource.getrlimit(resource.RLIMIT_NOFILE)
+        limit = max(soft, hard // 2)
+        resource.setrlimit(resource.RLIMIT_NOFILE, (limit, hard))
+
+    addr = uri_from_host_port('', None, 0)
+
+    loop = IOLoop.current()
+
+    services = {}
+    bokeh = False
+    with ignoring(ImportError):
+        from distributed.bokeh.scheduler import BokehScheduler
+        services[('bokeh', 0)] = (BokehScheduler, {})
+        bokeh = True
+
+    scheduler = Scheduler(loop=loop, services=services)
+    scheduler.start(addr)
+
+    install_signal_handlers(loop)
+
+    app_client.kv['dask.scheduler'] = scheduler.address
+
+    if bokeh:
+        bokeh_port = scheduler.services['bokeh'].port
+        bokeh_host = urlparse(scheduler.address).hostname
+        bokeh_address = 'http://%s:%d' % (bokeh_host, bokeh_port)
+
+        app_client.kv['dask.dashboard'] = bokeh_address
+
+    try:
+        loop.start()
+        loop.close()
+    finally:
+        scheduler.stop()
 
 
 if __name__ == '__main__':
--- dask_yarn/cli/dask_yarn_worker.py
+++ dask_yarn/cli/dask_yarn_worker.py
@@ -1,10 +1,61 @@
-import click
+from __future__ import print_function, division, absolute_import
 
-@click.command()
-@click.option('--memory-limit')
-@click.option('--nthreads', type=int, default=0)
-def main(memory_limit, nthreads):
-    print('hello from worker', memory_limit, nthreads)
+import argparse
+
+import skein
+from distributed import Nanny
+from distributed.utils import ignoring
+from distributed.cli.utils import install_signal_handlers
+from distributed.proctitle import (enable_proctitle_on_children,
+                                   enable_proctitle_on_current)
+from tornado.ioloop import IOLoop, TimeoutError
+from tornado import gen
+
+
+def main(args=None):
+    parser = argparse.ArgumentParser(prog="dask-yarn-worker",
+                                     description="Start a dask worker on YARN")
+    parser.add_argument("nthreads", type=int, help="Number of threads")
+    parser.add_argument("--memory_limit", help="Memory limit", default="auto")
+    kwargs = vars(parser.parse_args(args=args))
+    start_worker(**kwargs)
+
+
+def start_worker(nthreads, memory_limit="auto"):
+    enable_proctitle_on_current()
+    enable_proctitle_on_children()
+
+    services = {}
+    with ignoring(ImportError):
+        from distributed.bokeh.worker import BokehWorker
+        services[('bokeh', 0)] = (BokehWorker, {})
+
+    app_client = skein.ApplicationClient.from_current()
+
+    scheduler = app_client.kv.wait('dask.scheduler')
+
+    loop = IOLoop.current()
+
+    nanny = Nanny(scheduler, ncores=nthreads, services=services, loop=loop,
+                  memory_limit=memory_limit, worker_port=0)
+
+    @gen.coroutine
+    def close(signalnum):
+        nanny._close(timeout=2)
+
+    install_signal_handlers(loop, cleanup=close)
+
+    @gen.coroutine
+    def run():
+        yield nanny._start(None)
+        while nanny.status != 'closed':
+            yield gen.sleep(0.2)
+
+    try:
+        loop.run_sync(run)
+    except (KeyboardInterrupt, TimeoutError):
+        pass
+
 
 if __name__ == '__main__':
     main()
