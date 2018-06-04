2582dd3b9939bea03adc41864d10709174ae621e
Jim Crist <jiminy.crist@gmail.com>
2018-06-04 12:59:48 -0500

    Update `setup.py`

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup
 setup(name='dask-yarn',
       version='0.0.1',
       license='BSD',
-      description='Package conda environments for redistribution',
+      description='Deploy dask clusters on Apache YARN',
       packages=['dask_yarn', 'dask_yarn.cli'],
+      install_requires=['distributed', 'skein'],
       entry_points='''
         [console_scripts]
         dask-yarn-worker=dask_yarn.cli.dask_yarn_worker:main
