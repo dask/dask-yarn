0717117084aa2e3cde77e1d9707268192606a6df
Jim Crist <jiminy.crist@gmail.com>
2018-06-22 00:25:42 -0500

    Update config patch

--- dask_yarn/_patch.py
+++ dask_yarn/_patch.py
@@ -4,17 +4,20 @@ import os
 
 import dask.config
 
-if not hasattr(dask.config, 'DASK_CONFIG'):
+if not hasattr(dask.config, 'PATH'):
     # Old versions of dask would try to write the configuration files to the
     # same directory, no matter the value of `DASK_CONFIG`. For these we wrap
     # `dask.config.ensure_file`.
-    DASK_CONFIG = os.environ.get('DASK_CONFIG')
-    if DASK_CONFIG is None:
-        DASK_CONFIG = os.path.join(os.path.expanduser('~'), '.config', 'dask')
+    PATH = os.environ.get('DASK_CONFIG')
+    if PATH is None:
+        PATH = os.path.join(os.path.expanduser('~'), '.config', 'dask')
 
     _ensure_file = dask.config.ensure_file
 
-    def ensure_file(source, destination=DASK_CONFIG, comment=True):
-        return _ensure_file(source, destination=destination, comment=comment)
+    def ensure_file(source, destination=PATH, comment=True):
+        try:
+            return _ensure_file(source, destination=destination, comment=comment)
+        except OSError:
+            pass
 
     dask.config.ensure_file = ensure_file
