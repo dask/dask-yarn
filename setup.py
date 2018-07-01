640c72c03897f5a6e0dfdd3df528a49f9aa57359
Matthew Rocklin <mrocklin@gmail.com>
2018-07-01 15:19:38 -0400

    add requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
 setup(name='dask-yarn',
       version='0.0.1',
       license='BSD',
       description='Deploy dask clusters on Apache YARN',
       packages=['dask_yarn', 'dask_yarn.cli'],
       include_package_data=True,
-      install_requires=['distributed', 'skein'],
+      install_requires=install_requires,
       entry_points='''
         [console_scripts]
         dask-yarn-worker=dask_yarn.cli.dask_yarn_worker:main
