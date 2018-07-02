41dedd62773db7a86afc2fee663171f66cfe5959
Jim Crist <jiminy.crist@gmail.com>
2018-07-02 18:26:49 -0500

    Add versioneer  [skip ci]

--- dask_yarn/__init__.py
+++ dask_yarn/__init__.py
@@ -6,3 +6,7 @@ from . import config
 del _patch, config
 
 from .core import YarnCluster
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
+import versioneer
 
 with open('requirements.txt') as f:
     install_requires = f.read().strip().split('\n')
 
+with open('README.rst') as f:
+    long_description = f.read()
+
 setup(name='dask-yarn',
-      version='0.0.1',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       license='BSD',
       description='Deploy dask clusters on Apache YARN',
+      long_description=long_description,
       packages=['dask_yarn', 'dask_yarn.cli'],
       include_package_data=True,
       install_requires=install_requires,
